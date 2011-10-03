9204d06be51daa4dae91f4020ca9b194c1564646
Rainer Simon <rainer.simon@ait.ac.at>
2011-10-03 17:40:40 +0200

    Added 2-thumb-slider component

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 goog.provide('europeana.main');
 
+goog.require('goog.debug.ErrorHandler');
+goog.require('goog.ui.Component');
+goog.require('goog.ui.TwoThumbSlider');
 goog.require('goog.dom');
 goog.require('goog.net.Jsonp');
 goog.require('goog.text.LoremIpsum');
@@ -7,23 +10,31 @@ goog.require('goog.text.LoremIpsum');
 // var API_KEY = "IAVQBBDOQQ";
 var API_KEY = "IZHBRKKFDW";
 
-europeana.query = function(fromYear, toYear, fromLat, fromLon, toLat, toLon, startPage) {
-	return "http://acceptance.europeana.eu/api/opensearch.json?" +
-		"wskey=" + API_KEY +
-		"&startPage=" + startPage +
-		"&searchTerms=" + 
-		"enrichment_period_begin%3A[" + fromYear + "-01-01T00%3A00%3A00Z+TO+" + toYear + "-01-01T23%3A59%3A59Z]+AND+" +
-		"enrichment_period_end%3A[" + fromYear + "-01-01T00%3A00%3A00Z+TO+" + toYear + "-01-01T23%3A59%3A59Z]" +
-		"enrichment_place_latitude%3A[" + fromLat + "+TO+" + toLat + "]+AND+" +
-		"enrichment_place_longitude%3A[" + fromLon + "+TO+" + toLon + "]";
+europeana.query = function(fromYear, toYear, fromLat, fromLon, toLat, toLon,
+		startPage) {
+	return "http://acceptance.europeana.eu/api/opensearch.json?" + "wskey="
+			+ API_KEY + "&startPage=" + startPage + "&searchTerms="
+			+ "enrichment_period_begin%3A[" + fromYear
+			+ "-01-01T00%3A00%3A00Z+TO+" + toYear
+			+ "-01-01T23%3A59%3A59Z]+AND+" + "enrichment_period_end%3A["
+			+ fromYear + "-01-01T00%3A00%3A00Z+TO+" + toYear
+			+ "-01-01T23%3A59%3A59Z]" + "enrichment_place_latitude%3A["
+			+ fromLat + "+TO+" + toLat + "]+AND+"
+			+ "enrichment_place_longitude%3A[" + fromLon + "+TO+" + toLon + "]";
 }
 
 europeana.main = function() {
-  var jsonp = new goog.net.Jsonp(europeana.query(0, 2010, 0, -20, 80, 110, 1));
-  jsonp.send({}, function(data) {
-	  alert(data);	  
-  });
-}
+	var el = document.getElementById('s1');
+	var s = new goog.ui.TwoThumbSlider;
+	s.decorate(el);
+	s.addEventListener(goog.ui.Component.EventType.CHANGE, function() {
+		document.getElementById('out1').innerHTML = 'start: ' + s.getValue()
+				+ ' end: ' + (s.getValue() + s.getExtent());
+	});
 
+	// JSONP Europeana query codes sample
+	// var jsonp = new goog.net.Jsonp(europeana.query(0, 2010, 0, -20, 80, 110, 1));
+	// jsonp.send({}, function(data) { alert(data); });
+}
 
 window['main'] = europeana.main;
